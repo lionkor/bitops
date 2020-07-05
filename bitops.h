9c413fe98b7555e410a5997e48a521449c8d8ffc
Lion Kortlepel <development@kortlepel.com>
2020-07-05 23:38:38 +0200

    Fix some tests

--- bitops.h
+++ bitops.h
@@ -5,43 +5,49 @@ namespace bitops {
 
 /// Flips the bit at position n
 template<typename _IntT, typename _SmallIntT>
-inline _IntT& bit_flip(_IntT& i, _SmallIntT n) {
-    return i ^= 1 << (n - 1);
+inline void flip(_IntT& i, _SmallIntT n) {
+    i ^= 1 << (n - 1);
 }
 
 /// Sets the bit at position n
 template<typename _IntT, typename _SmallIntT>
-inline _IntT& bit_set(_IntT& i, _SmallIntT n) {
-    return i |= 1 << (n - 1);
+inline void set(_IntT& i, _SmallIntT n) {
+    i |= 1 << (n - 1);
 }
 
 /// Sets all bits in the mask in i
 template<typename _IntT>
-inline _IntT& bit_set_mask(_IntT& i, _IntT mask) {
-    return i |= mask;
+inline void set_mask(_IntT& i, _IntT mask) {
+    i |= mask;
+}
+
+/// Unsets all bits in the mask in i
+template<typename _IntT>
+inline void unset_mask(_IntT& i, _IntT mask) {
+    i &= ~mask;
 }
 
 /// Unsets the bit at position n
 template<typename _IntT, typename _SmallIntT>
-inline _IntT& bit_unset(_IntT& i, _SmallIntT n) {
-    return i &= ~(1 << (n - 1));
+inline void unset(_IntT& i, _SmallIntT n) {
+    i &= ~(1 << (n - 1));
 }
 
 /// Checks whether the bit at position n is set
 template<typename _IntT, typename _SmallIntT>
-inline constexpr bool is_bit_set(_IntT i, _SmallIntT n) {
+inline constexpr bool is_set(_IntT i, _SmallIntT n) {
     return i & (1 << (n - 1));
 }
 
 /// Checks whether the bit at position n is NOT set
 template<typename _IntT, typename _SmallIntT>
-inline constexpr bool is_bit_not_set(_IntT i, _SmallIntT n) {
-    return !is_bit_set(i, n);
+inline constexpr bool is_not_set(_IntT i, _SmallIntT n) {
+    return !is_set(i, n);
 }
 
 /// Checks whether all bits in the mask are set in i
 template<typename _IntT>
-inline constexpr bool is_bitmask_set(_IntT i, _IntT mask) {
+inline constexpr bool is_mask_set(_IntT i, _IntT mask) {
     return (i & mask) == mask;
 }
 
